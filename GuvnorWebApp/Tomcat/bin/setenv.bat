9e72927dc58d147ee6eb2dfbc331f1358bbd1aa5
barides <avi.barides@gmail.com>
2015-01-15 08:57:15 +0200

    Guvnor web app setup
    Guvnor web app setup

---
+++ GuvnorWebApp/Tomcat/bin/setenv.bat
@@ -0,0 +1,2 @@
+JAVA_OPTS="-Xms2048m -Xmx2048m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled"
+set CLASSPATH=%CLASSPATH%;"D:\Dev\drools\libs\*";
